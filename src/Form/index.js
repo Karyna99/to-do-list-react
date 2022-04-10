9bd39763cc85260b52c679f7faef3725e02bc061
Karyna Krapiunitskaya <krapinah@gmail.com>
2022-04-10 14:31:46 +0300

    adding new task interaction

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,10 +1,28 @@
+import { useState } from "react";
 import "./style.css";
 
-const Form = () =>(
-    <form className="form">
-        <input autoFocus placeholder="Co jest do zrobienia?" className="form__input" type="text"/>
-        <button className="form__button">Dodaj zadanie</button>
-    </form>
-);
+const Form = ({ addNewTask }) => {
+    const[newTask, setNewTask] = useState("");
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        addNewTask(newTask.trim());
+        setNewTask("");
+    };
+
+    return (
+        <form className="form" onSubmit={onFormSubmit}>
+            <input 
+            value={newTask}
+            autoFocus 
+            placeholder="Co jest do zrobienia?" 
+            className="form__input" 
+            type="text"
+            onChange={({ target }) => setNewTask(target.value)}
+            />
+            <button className="form__button">Dodaj zadanie</button>
+        </form>
+    );
+};
 
 export default Form;
